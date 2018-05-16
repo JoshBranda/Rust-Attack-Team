ab650f701186ce6e2ec8dab5e3549b1358135a77
Josh Sander <josander@pdx.edu>
2018-05-16 01:30:21 -0700

    Basic moving block implemented

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,104 @@ for license terms.
 
 extern crate ggez;
 
+use ggez::event::{Keycode, Mod};
 use ggez::{GameResult, Context};
-use ggez::graphics::{self, DrawMode, Point2};
+use ggez::graphics::{self, set_color, Color, DrawMode, Point2};
 use ggez::conf;
 use ggez::event;
+
+//Square size of blank graphic.  Will be useless with Sprites.
+const SQUARE_SIZE: f32 = 10.0;
 
 // Screen dimensions. Currently portrait mode.
 const WIN_W: u32 = 400;
 const WIN_H: u32 = 700;
 
+struct Square {
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+    colour: Color,
+}
+
+impl Square {
+    fn new(_ctx: &mut Context, x: f32, y: f32, width: f32, height: f32, colour: Color) -> Square {
+        Square {
+            x: x,
+            y: y,
+            width: width,
+            height: height,
+            colour: colour,
+        }
+    }
+
+    pub fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        set_color(ctx, self.colour)?;
+        let square = graphics::Rect::new(self.x, self.y, self.width, self.height);
+        graphics::rectangle(ctx, DrawMode::Fill, square)?;
+        Ok(())
+    }
+}
+
+struct Crab {
+    form: Square,
+}
+
+impl Crab {
+    fn new(ctx: &mut Context) -> Crab {
+        Crab {
+            form: Square::new(
+                ctx,
+                5.0 * SQUARE_SIZE,
+                WIN_H as f32 - 1.0 * SQUARE_SIZE,
+                SQUARE_SIZE,
+                SQUARE_SIZE,
+                Color::new(0.0, 1.0, 0.0, 1.0),
+            )
+        }
+    }
+
+    pub fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        self.form.draw(ctx)?;
+        Ok(())
+    }
+
+    pub fn move_up(&mut self) {
+        if self.form.y - SQUARE_SIZE > 0.0 {
+            self.form.y -= SQUARE_SIZE;
+        }
+    }
+    pub fn move_down(&mut self) {
+        if self.form.y + SQUARE_SIZE < WIN_H as f32 {
+            self.form.y += SQUARE_SIZE;
+        }
+    }
+    pub fn move_right(&mut self) {
+        if self.form.x + SQUARE_SIZE < WIN_W as f32 - SQUARE_SIZE {
+            self.form.x += SQUARE_SIZE;
+        }
+    }
+    pub fn move_left(&mut self) {
+        if self.form.x - SQUARE_SIZE > 0.0 {
+            self.form.x -= SQUARE_SIZE;
+        }
+    }
+}
+
 // Contains properties to track during gameplay
 // In this example it is only tracking the x coord of the orb
 struct MainState {
     pos_x: f32,
+    player: Crab,
 }
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+        let s = MainState {
+            pos_x: 0.0,
+            player: Crab::new(_ctx)
+        };
         Ok(s)
     }
 }
@@ -42,8 +122,22 @@ impl event::EventHandler for MainState {
                          Point2::new(self.pos_x, 350.0),
                          10.0,
                          2.0)?;
+
+        self.player.draw(ctx)?;
+
         graphics::present(ctx);
         Ok(())
+    }
+
+    fn key_down_event(&mut self, _ctx: &mut ggez::Context, keycode: Keycode, _: Mod, _: bool) {
+        match keycode {
+            Keycode::Up => self.player.move_up(),
+            Keycode::Down => self.player.move_down(),
+            Keycode::Right => self.player.move_right(),
+            Keycode::Left => self.player.move_left(),
+
+            _ => {}
+        }
     }
 }
 
