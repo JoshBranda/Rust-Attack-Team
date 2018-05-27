bf468309b6ca4b288b7723eac65a1aae3d24f6e9
Josh Sander <josander@pdx.edu>
2018-05-26 21:15:15 -0700

    Adding Menu struct

--- src/main.rs
+++ src/main.rs
@@ -131,52 +131,9 @@ impl event::EventHandler for MainState {
         graphics::clear(ctx);
 
         if self.main_menu {
-            //Draw Crabber name upper-middle
-            let game_name = format! {"CRABBER"};
-            let font = graphics::Font::new(ctx, "/game_over.ttf", 56).unwrap();
-            let name_text = graphics::Text::new(ctx, &game_name, &font)?;
-            let horizontal: f32 = WIN_W as f32 / 2.0 - name_text.width() as f32 / 2.0;
-            let vertical: f32 = WIN_H as f32 / 2.0 + SQUARE_SIZE * 1.0;
-            let dest_point = graphics::Point2::new(horizontal, vertical);
-            graphics::draw(ctx, &name_text, dest_point, 0.0)?;
+            let mut menu_graphics = Menu{};
+            menu_graphics.draw(ctx, menu_graphics);
 
-            //Draw the Crab logo
-            let image_big_crab = graphics::Image::new(ctx, "/crab.png")?;
-            let horizontal_crab: f32 = WIN_W as f32 / 2.0 - image_big_crab.width() as f32 / 2.0;
-            let vertical_crab: f32 = vertical - SQUARE_SIZE * 8.0;
-            let dest_point = graphics::Point2::new(horizontal_crab, vertical_crab);
-            graphics::draw(ctx, &image_big_crab, dest_point, 0.0)?;
-
-            //Draw Start option
-            let start = format! {"Start"};
-            let font_start = graphics::Font::new(ctx, "/game_over.ttf", 20).unwrap();
-            let start_text = graphics::Text::new(ctx, &start, &font_start)?;
-            let horizontal2: f32 = WIN_W as f32 / 2.0 - start_text.width() as f32 / 2.0;
-            let vertical2: f32 = WIN_H as f32 / 2.0 + SQUARE_SIZE * 5.0;
-            let dest_point = graphics::Point2::new(horizontal2, vertical2);
-            graphics::draw(ctx, &start_text, dest_point, 0.0)?;
-
-            //Draw Scores option
-            let scores = format! {"Scores"};
-            let font_score = graphics::Font::new(ctx, "/game_over.ttf", 20).unwrap();
-            let score_text = graphics::Text::new(ctx, &scores, &font_score)?;
-            let horizontal3: f32 = WIN_W as f32 / 2.0 - score_text.width() as f32 / 2.0;
-            let vertical3: f32 = vertical2 + SQUARE_SIZE;
-            let dest_point = graphics::Point2::new(horizontal3, vertical3);
-            graphics::draw(ctx, &score_text, dest_point, 0.0)?;
-
-            //Figure out where the crab selector goes
-            if self.selection == 0 {
-                let image_small_crab = graphics::Image::new(ctx, "/tiny_crab.png")?;
-                let horizontal_small_crab: f32 = horizontal2 - 30.0;
-                let dest_point = graphics::Point2::new(horizontal_small_crab, vertical2 + 10.0);
-                graphics::draw(ctx, &image_small_crab, dest_point, 0.0)?;
-            } else if self.selection == 1 {
-                let image_small_crab = graphics::Image::new(ctx, "/tiny_crab.png")?;
-                let horizontal_small_crab: f32 = horizontal3 - 30.0;
-                let dest_point = graphics::Point2::new(horizontal_small_crab, vertical3 + 10.0);
-                graphics::draw(ctx, &image_small_crab, dest_point, 0.0)?;
-            }
         } else {
 
             //Draw background
